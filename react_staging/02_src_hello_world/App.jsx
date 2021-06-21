d28d592fdcae63007981dc6aa8b197a49c0b9d69
fanx <708931190@qq.com>
2021-06-21 10:02:29 +0800

    React课程学习项目

--- react_staging/02_src_hello_world/App.jsx
+++ react_staging/02_src_hello_world/App.jsx
@@ -1,7 +1,7 @@
 //创建“外壳”组件App
 import React,{Component} from 'react'
-import Hello from "./02_src_hello_world/components/Hello";
-import Welcome from "./02_src_hello_world/components/Welcome";
+import Hello from "../02_src_hello_world/components/Hello";
+import Welcome from "../02_src_hello_world/components/Welcome";
 
 //创建并暴露App组件
 export default class App extends Component{
